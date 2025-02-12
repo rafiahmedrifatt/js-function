5e5673e344c62925a225a59e0684f9955425e3bc
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-02-12 23:41:25 +0600

    params added

--- function.js
+++ function.js
@@ -1,1 +1,25 @@
 const number = 45;
+
+// function declaration
+function fanOffKor() {
+  console.log("bosha theke uthe dara");
+  console.log("walk towards switch");
+  console.log("click the switch to off the fan");
+}
+
+function brushTeeth() {
+  console.log("pick up the brush");
+  console.log("add paste");
+  console.log("2 mint ghosho and ghosho and kuli koro");
+}
+
+// call the function
+fanOffKor();
+brushTeeth();
+console.log("-------------");
+console.log("-------------");
+console.log("-------------");
+console.log("-------------");
+console.log("-------------");
+console.log("tom tom tomato");
+brushTeeth();
---
+++ parameter.js
@@ -0,0 +1,7 @@
+function vaatkhao() {
+  console.log("hand wash kore asho");
+  console.log("plate e khabar naw");
+  console.log("ekhon khao");
+}
+
+vaatkhao();
